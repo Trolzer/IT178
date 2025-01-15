e06377f11c16e542eadb5a9ae9bd065c73707644
Ethan Causa <ejcausa@ilstu.edu>
2025-01-15 10:41:25 -0600

    v1

--- src/ArrayReview/Student.java
+++ src/ArrayReview/Student.java
@@ -1,4 +1,21 @@
 package ArrayReview;
 
 public class Student {
+
+    private final String name;
+    private final int grade;
+
+    public Student(String name, int grade){
+        this.name = name;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + grade;
+    }
+
+    public int getGrade(){return grade;}
+    public String getName(){return name;}
+
 }
--- src/ArrayReview/StudentDataReader.java
+++ src/ArrayReview/StudentDataReader.java
@@ -1,4 +1,28 @@
 package ArrayReview;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class StudentDataReader {
+
+    public Student readDataFromFile(String filename){
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            Student [] studentList = new Student[4];
+            while((line=reader.readLine())!=null){
+                
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found!");
+        } catch (IOException e) {
+            System.out.println("IO error!");
+        }
+
+    }
+
 }
