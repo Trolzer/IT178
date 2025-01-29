cab6e7c045b3a1dea699caafe396107a48111e37
Trolzer <39714370+Trolzer@users.noreply.github.com>
2025-01-28 19:54:09 -0600

    arraylistLab fixes

--- src/ArrayListLab/MakeCodesList.java
+++ src/ArrayListLab/MakeCodesList.java
@@ -6,10 +6,18 @@ import java.util.Scanner;
 public class MakeCodesList {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        int numIter;
+        int numIter = 0;
         int i;
         /* Variable declarations go here */
-        numIter = scnr.nextInt();
+        try {
+            numIter = Integer.parseInt(scnr.next());
+        }catch (NumberFormatException e){
+            System.out.println("invalid input, defaulting to 0");
+        }
+        if (numIter < 0){
+            numIter = 0;
+            System.out.println("negative input, defaulting to 0");
+        }
         ArrayList<Character> codesList = new ArrayList<Character>();
         /* Your code goes here */
         for (i = 0; i < numIter; i++){
