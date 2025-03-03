b297da824818cbd943008ece4b68cd19d18d7111
Trolzer <39714370+Trolzer@users.noreply.github.com>
2025-03-03 15:22:32 -0600

    SortingAlgorithmMain

--- src/SortingAlgorithmsProgram/SortingAlgorithmMain.java
+++ src/SortingAlgorithmsProgram/SortingAlgorithmMain.java
@@ -1,7 +1,8 @@
 package SortingAlgorithmsProgram;
 
+import javax.crypto.Cipher;
 import java.lang.reflect.Array;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class SortingAlgorithmMain {
@@ -9,18 +10,31 @@ public class SortingAlgorithmMain {
     public static void main(String[] args) {
 
 
-        //100 int array
-        int[] arrayInt100 = randomArray(10);
+            //100,1000,10000 random int arrays
+            int[] arrayInt100 = randomArray(100);
+            int[] arrayInt1000 = randomArray(1000);
+            int[] arrayInt10000 = randomArray(10000);
 
-        BubbleSort bubbleSort = new BubbleSort(arrayInt100);
-        SelectionSort selectionSort = new SelectionSort(arrayInt100);
-        InsertionSort insertionSort = new InsertionSort(arrayInt100);
-        MergeSort mergeSort = new MergeSort(arrayInt100);
-        QuickSort quickSort = new QuickSort(arrayInt100);
+            //100,1000,10000 random int arrays sorted
+            int[] arrayInt100Sorted = Arrays.copyOf(arrayInt100,100);
+            Arrays.sort(arrayInt100Sorted);
+            int[] arrayInt1000Sorted = Arrays.copyOf(arrayInt1000,1000);
+            Arrays.sort(arrayInt1000Sorted);
+            int[] arrayInt10000Sorted = Arrays.copyOf(arrayInt10000, 10000);
+            Arrays.sort(arrayInt10000Sorted);
 
-        quickSort.printElements();
-        quickSort.sort();
-        quickSort.printElements();
+
+        //sort timing
+            testSorts(arrayInt100);
+            testSorts(arrayInt1000);
+            testSorts(arrayInt10000);
+
+            //test with sorted arrays
+            System.out.println("------------SORTED ARRAY TEST------------");
+            testSorts(arrayInt100Sorted);
+            testSorts(arrayInt1000Sorted);
+            testSorts(arrayInt10000Sorted);
+
 
 
     }
@@ -30,4 +44,41 @@ public class SortingAlgorithmMain {
         return new Random().ints(n,0,1001).toArray();
     }
 
+    //helper for timing sorts
+    public static void testSorts(int[] arr){
+        BubbleSort bubbleSort = new BubbleSort(arr);
+        InsertionSort insertionSort = new InsertionSort(arr);
+        MergeSort mergeSort = new MergeSort(arr);
+        QuickSort quickSort = new QuickSort(arr);
+        SelectionSort selectionSort = new SelectionSort(arr);
+
+        long startTime = System.nanoTime();
+        bubbleSort.sort();
+        long endTime = System.nanoTime();
+        System.out.println("Bubble Sort " + arr.length + ": " + (endTime - startTime) + "ns");
+
+        startTime = System.nanoTime();
+        insertionSort.sort();
+        endTime = System.nanoTime();
+        System.out.println("Insertion Sort " + arr.length + ": " + (endTime - startTime) + "ns");
+
+        startTime = System.nanoTime();
+        mergeSort.mergeSort();
+        endTime = System.nanoTime();
+        System.out.println("Merge Sort " + arr.length + ": " + (endTime - startTime) + "ns");
+
+        startTime = System.nanoTime();
+        quickSort.sort();
+        endTime = System.nanoTime();
+        System.out.println("Quick Sort " + arr.length + ": " + (endTime - startTime) + "ns");
+
+        startTime = System.nanoTime();
+        selectionSort.sort();
+        endTime = System.nanoTime();
+        System.out.println("Selection Sort " + arr.length + ": " + (endTime - startTime) + "ns" + "\n");
+
+
+
+
+    }
 }
